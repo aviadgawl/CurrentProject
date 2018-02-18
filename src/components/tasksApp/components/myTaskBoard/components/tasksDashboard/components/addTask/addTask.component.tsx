77c3cb4d26192f8824e9d8a100295b037476c43f
aviadgawl <aviadgawl@gmail.com>
2018-02-18 16:04:34 +0200

    starting working on the add task

---
+++ src/components/tasksApp/components/myTaskBoard/components/tasksDashboard/components/addTask/addTask.component.tsx
@@ -0,0 +1,18 @@
+import './addTask.style.css';
+import * as React from 'react';
+
+interface addTaskProps {};
+interface addTaskStatus {};
+
+export default class AddTask extends React.Component<addTaskProps , addTaskStatus>{
+    constructor(props:addTaskProps){
+        super(props);
+    }
+
+    render(){
+        return <div>
+            
+        </div>
+    }
+}
+
